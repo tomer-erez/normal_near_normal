d61a6269256f6fec65add7df5e60bc969573fba8
Tomer Erez <tomererez1998@gmail.com>
2025-09-12 00:15:33 +0300

    reset readme

--- core/arguments.py
+++ core/arguments.py
@@ -4,7 +4,8 @@ import torch
 def get_args():
     parser = argparse.ArgumentParser(description='CT + Text Classification')
     
-    # General training stuff
+    ##################################################################
+    # ---------------------General training stuff---------------------
     parser.add_argument('--epochs', type=int, default=20)
     parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--lr_gamma', type=float, default=0.8)
@@ -13,13 +14,13 @@ def get_args():
     parser.add_argument('--resume', type=str, default=None, help='Checkpoint path to resume training')
     parser.add_argument('--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu')
     parser.add_argument('--seed', type=int, default=42)
-
-    # CT pre-processing stuff
+    ##################################################################
+    # ---------------------CT pre-processing stuff--------------------
     parser.add_argument('--d_target', type=int, default=40, help="number of ct slices to pad/truncate to")
     parser.add_argument('--brain_window_function_center', type=int, default=40)
     parser.add_argument('--brain_window_function_width', type=int, default=80)
-    
-    # Dataset stuff
+    ##################################################################
+    # --------------------------Dataset stuff-------------------------
     parser.add_argument('--sinoCT_dataset_path', type=str, default=r"C:\Users\tomer.erez\Desktop\code_projects\normal_near_normal\stanford_data\ctsinogram\head_ct_dataset_anon", 
     help='in the sinoCT dataset,' \
     'path to the folder which holds: batch_0, batch_1, batch_2... etc. might be called head_ct_dataset_anon' \
@@ -30,12 +31,12 @@ def get_args():
     parser.add_argument('--sinoCT_csv_path', type=str, default=r"C:\Users\tomer.erez\Desktop\code_projects\normal_near_normal\stanford_data\labels.csv", 
     help='in the sinoCT dataset,' \
     'path to the sinoCT labels csv')
-
-    # Logging stuff
+    ##################################################################
+    # --------------------------Logging stuff-------------------------
     parser.add_argument('--log_path', type=str, default=r'.\log_file.log')
+    ##################################################################
 
 
 
-    args = parser.parse_args()
 
-    return args
+    return parser.parse_args()
