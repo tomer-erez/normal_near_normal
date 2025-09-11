49afa8703360daf8767eb534030bfa9887567e62
Tomer Erez <tomererez1998@gmail.com>
2025-09-11 17:38:27 +0300

    bash helpers update

--- bash_helpers.sh
+++ bash_helpers.sh
@@ -1,3 +1,2 @@
 # windows command using azcopy to download all .dcm files from the standford data, exclude teh .npy sinograms:
-# azcopy copy "stanford_data_link" "target_download_path" --recursive=true --include-pattern="*.dcm"
-azcopy copy "https://aimistanforddatasets01.blob.core.windows.net/ctsinogram?sv=2019-02-02&sr=c&sig=PKqiCpV3CDYgb6DfKdmizD6rzhdPpPymss75qnbtekg%3D&st=2025-09-11T13%3A57%3A25Z&se=2025-10-11T14%3A02%3A25Z&sp=rl" "the\path\you\want\to\download\to" --recursive=true --include-pattern="*.dcm"
+azcopy copy "stanford_data_link" "target_download_path" --recursive=true --include-pattern="*.dcm"
