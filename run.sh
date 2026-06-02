b3d150a2ffbf95c9fbfd4946ba898e0e1fa78fd4
tomer erez <tomererez1998@gmail.com>
2026-06-02 14:58:53 +0300

    w

--- run.sh
+++ run.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-name="clip_w10_margin-50_negative_aware"
+name="caption_mode_all_clip"
 set -e
 cd "$(dirname "$0")"
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate thesis_clip
 mkdir -p logs experiments/$name
 tmux new-session -d -s $name "
-    CUDA_VISIBLE_DEVICES=1,2 torchrun --nproc_per_node=2 --master_port=29505 train_lora.py \
+    CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 --master_port=29507 train_lora.py \
         --base-model ViT-B-32 \
         --pretrained openai \
         --precision bf16 \
         --lr 1e-4 \
         --min-lr 1e-8 \
-        --batch-size 50 \
+        --batch-size 128 \
         --nan-mode ignore \
-        --negative-weight 10 \
-        --negative-margin -50 \
         --loss clip \
-        --match-mode negative_aware \
+        --caption-mode all \
+        --match-mode single_label \
         --lora-r 16 \
         --lora-alpha 16 \
         --output-dir ./experiments/$name \
